79470c593364cfac01df453cfb28364b2a2d999c
PhucDuong-SSS <phucdndev@gmail.com>
2021-05-12 13:23:03 +0700

    [add] bind to link

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import Home from './components/Home.vue';
 import Register from './components/Register.vue';
 import Login from './components/Login.vue';
 import Student from './components/student/Student.vue';
+import Redirect from './components/Redirect.vue';
 
 export const routes = [
     {path:'/', name:'homepage', component:Home},
     {path:'/register', name:'register', component:Register},
     {path:'/login', name: 'login', component: Login},
-    {path:'/student/:idd', name:'student', component:Student}
+    {path:'/student/:idd', name:'student', component:Student},
+    {path:'/redirect', name:'redirect', component:Redirect}
 
 ];
